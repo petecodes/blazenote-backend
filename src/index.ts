559cf1e693398900415bd543fd6fde9716337aec
Pete <peteprojectsnow@gmail.com>
2025-08-05 13:13:14 +0800

    Update index.ts
    added FE domain

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = new Hono();
 // Important! No trailing slash
 const allowedOrigins = new Set([
   "http://localhost:5173",
-  "https://<your-frontend-domain>.<tld>",
+  "https://10e6fbd0.blazenote-frontend-ay1.pages.dev/",
 ]);
 
 // https://hono.dev/docs/middleware/builtin/cors
